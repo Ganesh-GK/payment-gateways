30a57bd624f87cf1d4a25aac396959b9e41f066b
Ganesh <ganesh@sparkouttech.com>
2021-10-30 19:22:25 +0530

    stripe-functions-add

--- src/App/Controllers/StripePaymentController.php
+++ src/App/Controllers/StripePaymentController.php
@@ -3,6 +3,7 @@
 namespace Sparkouttech\PaymentGateway\App\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Log;
 use Stripe;
 
 class StripePaymentController extends Controller
@@ -17,13 +18,47 @@ class StripePaymentController extends Controller
         Stripe\Stripe::setApiKey(config('payment-gateway.stripe_secret'));
     }
 
-    public function createCharge($amount, $stripeToken, $currency = "usd"){
+    public function createCustomer(array $user)
+    {
+        return Stripe\Customer::create([
+            'email' => $user['email'],
+            'name' => $user['name'],
+            'phone' => $user['phone'],
+            'description' => $user['email']
+        ]);
+    }
 
-        Stripe\Charge::create ([
+    public function createCard(array $attributes)
+    {
+        return Stripe\Card::create($attributes);
+    }
+
+    public function createCharge($amount, $stripeToken, $currency = "usd")
+    {
+        return Stripe\Charge::create ([
             "amount" => $amount * 100,
             "currency" => $currency,
             "source" => $stripeToken,
             "description" => "This payment is tested purpose."
         ]);
     }
+
+    public function createSetupIntent(array $attributes)
+    {
+        return Stripe\SetupIntent::create([
+            'customer' => $attributes['customer'],
+            'usage' => "on_session"
+        ]);
+    }
+
+    public function createPaymentIntent(array $attributes)
+    {
+        return Stripe\PaymentIntent::create([
+            'amount' => ($attributes['amount'] * 100),
+            'currency' => $attributes['currency'],
+            'customer' => $attributes['customer'],
+            'payment_method' => $attributes['payment_method']
+        ]);
+    }
+
 }
